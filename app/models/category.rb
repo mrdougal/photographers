f834fe3bcbc0fc992ff94ef23608cb231ce8d543
Dougal MacPherson <hello@newfangled.com.au>
2009-12-29 13:11:39 +1100

    Adding permalink attributes to category.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,17 @@
 class Category < ActiveRecord::Base
+  
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  
+  acts_as_url :name, :url_attribute => :permalink, :sync_url => true
+  
+  
+  def to_s
+    name
+  end
+  
+  def to_param
+    permalink
+  end
+  
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,25 +1,31 @@
 require 'spec_helper'
 
 describe Category do
+  
+  before(:each) do
+    @category = Factory :category
+  end
 
   it "should require a name" do
-    
+    lambda { Factory :category, :name => nil }.should raise_error(ActiveRecord::RecordInvalid)
   end
   
   describe "permalink" do
     
     it "should have a peramlink" do
-      
+      @category.permalink.should_not be_blank    
     end
     
     it "should update the permalink to match the name" do
-      
+      @category.name = 'Something different'
+      @category.save
+      @category.permalink.should == 'something-different'
     end
     
   end
   
   it "doesn't require a description" do
-    
+    lambda { Factory :category, :description => nil }.should_not raise_error
   end
   
   it "should belong to a site" do
