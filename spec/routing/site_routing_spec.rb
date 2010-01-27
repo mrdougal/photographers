55e7910192f2a90fac48ba73aacdb21c9117ef17
Dougal MacPherson <hello@newfangled.com.au>
2010-01-27 18:12:11 +1100

    Tests for the rescue methods called internally by apache

--- spec/routing/site_routing_spec.rb
+++ spec/routing/site_routing_spec.rb
@@ -8,18 +8,6 @@ describe SiteController do
       { :get => "/" }.should route_to(:controller => "site", :action => "homepage")
     end
 
-    it "recognises and generates params for #photos" do
-      { :get => "/photos" }.should route_to(:controller => "site", :action => "photos")
-    end
-
-    it "recognises and generates params for #photo" do
-      { :get => "/photos/1" }.should route_to(:controller => "site", :action => "photo", :permalink => "1" )
-    end
-    
-    it "recognises and generates params for #tagged" do
-      { :get => "/photos/tagged/1" }.should route_to(:controller => "site", :action => "tagged_photos", :tag => "1" )
-    end
-    
     it "recognises #about" do
       { :get => "/about" }.should route_to(:controller => "site", :action => "about")
     end
@@ -27,6 +15,34 @@ describe SiteController do
     it "recognised #contact" do
       { :get => "/contact" }.should route_to(:controller => "site", :action => "contact" )
     end
+
+    describe "photos" do
+      
+      it "recognises and generates params for #photos" do
+        { :get => "/photos" }.should route_to(:controller => "site", :action => "photos")
+      end
+
+      it "recognises and generates params for #photo" do
+        { :get => "/photos/1" }.should route_to(:controller => "site", :action => "photo", :permalink => "1" )
+      end
+    
+      it "recognises and generates params for #tagged" do
+        { :get => "/photos/tagged/1" }.should route_to(:controller => "site", :action => "tagged_photos", :tag => "1" )
+      end
+    end
+
+
+    describe "rescues (called by Apached)" do
+      it "recognises and generates params for #not_found" do
+        { :get => "/rescue/404" }.should route_to(:controller => "site", :action => "not_found" )
+      end
+
+      it "recognises and generates params for #not_found" do
+        { :get => "/rescue/403" }.should route_to(:controller => "site", :action => "not_allowed" )
+      end
+
+    end
+    
 
   end
 end
