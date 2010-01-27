b999da23d2ef7dff9cf7721171a86c686241c782
Dougal MacPherson <hello@newfangled.com.au>
2010-01-28 10:28:08 +1100

    Refinement of admin layout. Inclusion of visual elements. (images and css)

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,19 @@
+module AdminHelper
+  
+  def busy_image(id='busy')
+    image_tag 'admin/icons/busy.gif', :id => id, :style => 'display: none;'
+  end
+  
+
+  # Only match if we are the current path
+  def admin_current_class(url)
+    'current' if request.path == url
+  end 
+  
+  def admin_current_or_active_class(url)
+    
+    # return 'current' if request.path == url
+    'current' if request.path =~ Regexp.new("#{url}/*")
+  end 
+  
+end
