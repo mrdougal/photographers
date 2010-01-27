a1feb038cb8f29beed5a205663e4b8215048fad3
Dougal MacPherson <hello@newfangled.com.au>
2010-01-27 17:36:40 +1100

    Adding in title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def title(msg)
+                             
+    # We have a new string so that msg isn't effected
+    s = String.new(msg.to_s)
+    return if s.blank?
+
+    content_for :title do
+      
+      # Add in a spacer
+      s << ' - '
+    end
+  end
+  
+  
 end
