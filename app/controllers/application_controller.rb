271aff3d5997bfa826a83556c95166971128b89f
Dougal MacPherson <hello@newfangled.com.au>
2010-01-27 18:09:37 +1100

    Adding in rescues for ActiveRecord::NotFound and also not allowed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+
+  rescue_from ActiveRecord::RecordNotFound, :with    => :not_found
+  rescue_from ActionController::RoutingError, :with  => :not_found
+  rescue_from ActionController::UnknownAction, :with => :not_foundend
+
+
+  # For 404, which can be called via Apache care of it's config
+  def not_found
+    render :template => 'shared/rescues/not_found', :status => 404 
+  end
+           
+  # For 403, (which will be called via Apache care of it's config)
+  def not_allowed
+    render :template => 'shared/rescues/not_allowed', :status => 403 
+  end
+
 end
