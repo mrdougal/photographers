1301c3cbe9b3cc125c3e1f3878fb11b463215fca
Dougal MacPherson <hello@newfangled.com.au>
2010-02-01 11:01:14 +1100

    Script to attach javascript prompts to links pointing out of the admin site and logout.

---
+++ public/javascripts/admin/application.js
@@ -0,0 +1,26 @@
+// Javascript for when we're in the admin section of the website
+
+$(document).ready(function() {
+	// Stuff to do as soon as the DOM is ready;
+	attach_confirmation_to_public_links();
+
+});
+
+
+function attach_confirmation_to_public_links () {
+
+	// Find all links, and filter out the ones which don't point to within the admin section
+	$('a').filter(':not([href*="admin"])').each(function() {
+
+		// This will attach a javascript prompt for when the link is clicked
+		$(this).bind('click', function() {
+
+			// If the link is to the logout, attach a different message
+			if ($(this).attr('pathname') == '/logout') {
+				return confirm('You sure you want to logout?')
+			} else{
+				return confirm('This will take you out of the admin section of the website');
+			};
+		});
+	})
+}
