9160c3934eb486cf0d6abc810bdefd8e75763a80
Dougal MacPherson <hello@newfangled.com.au>
2010-02-02 12:02:19 +1100

    Remove warnings when rendering the slideshow

--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -3,45 +3,6 @@ require 'spec_helper'
 describe SiteController do
 
 
-  describe "photos" do
-    
-    before(:all) do
-      @photos = Factory :photo, :name => "cheese", :tag_list => "bacon" 
-    end
-    
-    after(:all) do
-      @photos.destroy
-    end
-   
-    it "renders the photos template" do
-      get :photos
-      response.should render_template('photos')
-    end
-    
-    it "renders the photo template" do
-      get :photo, :permalink => "cheese" 
-      response.should render_template('photo')
-    end
-    
-    it "renders 404 if the photo doesn't exists" do
-      get :photo, :permalink => "non-existant"
-      response.should render_template('shared/rescues/not_found') 
-    end
-
-    
-    it "renders the photos tagged template (as there are photos tagged with the term)" do
-      get :photos_tagged, :tag => "bacon" 
-      response.should render_template('photos_tagged')
-    end
-    
-    it "render empty (as there are no photo tagged with our term)" do
-      get :photos_tagged, :tag => "cheese"
-      response.should render_template('photos_tagged_empty') 
-    end
-    
-  end
-
-
   describe "other resources" do
 
     describe "homepage" do
