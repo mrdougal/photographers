3c081866ca39c88dd7943df1f9768695347e1657
Dougal MacPherson <hello@newfangled.com.au>
2010-06-23 20:34:11 +1000

    Added before filter to redirect user if already logged in and trying to access the login page

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UserSessionsController < AdminController
 
+  before_filter :check_not_logged_in, :only => [:new] 
   skip_filter :require_user, :except => [:destroy, :dashboard]
 
   
@@ -7,10 +8,12 @@ class Admin::UserSessionsController < AdminController
     redirect_to login_path
   end
 
+  # GET /login
   def new
     @user_session = UserSession.new
   end
 
+  # POST /login
   def create
 
     @user_session = UserSession.new(params[:user_session])
@@ -22,6 +25,7 @@ class Admin::UserSessionsController < AdminController
     end
   end
 
+  # /logout
   def destroy
     @user_session = UserSession.find(params[:id])
     @user_session.destroy
@@ -42,6 +46,10 @@ class Admin::UserSessionsController < AdminController
   def redirect_back_or_default(default = admin_path)
     redirect_to(session[:return_to] || default)
     session[:return_to] = nil
+  end
+  
+  def check_not_logged_in
+    redirect_to admin_path if current_user
   end
 
 
