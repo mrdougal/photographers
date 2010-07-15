213b9faeb45e039a457f088f093f470c6a780439
Dougal MacPherson <hello@newfangled.com.au>
2010-07-16 09:46:45 +1000

    ImageMagick now installed via homebrew (on dev machine) So don't need define path to IM binary

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,11 +1,8 @@
 
-
+# ImageMagick on my local box, has been installed via Homebrew
+# Therefore is installed into /usr/local (which is already in our path)
 
 if ['development','test'].member? Rails.env
-  
-  # Because ImageMagick is installed in a different place on my dev/testing box
-  # We're now using MacPorts (got desperate)
-  Paperclip.options[:command_path] = '/opt/local/bin' 
   
   # Turn on additional logging
   Paperclip.options[:swallow_stderr] = false
